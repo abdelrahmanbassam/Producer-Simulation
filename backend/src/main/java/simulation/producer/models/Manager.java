3e37bdc8da32e1a95b8cf4d5c3e0e1bcb3c40995
MouradMahgoub <mouradmahgoub710@gmail.com>
2024-01-20 04:05:07 +0200

    Add Manager, Queue, RequestObject, and Observer interfaces
    This commit adds the Manager, Queue, RequestObject, and Observer interfaces to the codebase. These interfaces provide the necessary structure and functionality for managing queues, products, and machines in the simulation.

---
+++ backend/src/main/java/simulation/producer/models/Manager.java
@@ -0,0 +1,12 @@
+package simulation.producer.models;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class Manager {
+    ArrayList<Queue> queues = new ArrayList<Queue>();
+    ArrayList<Machine> machines = new ArrayList<Machine>();
+    ArrayList<Product> products = new ArrayList<Product>();
+
+    
+}
--- backend/src/main/java/simulation/producer/models/Queue.java
+++ backend/src/main/java/simulation/producer/models/Queue.java
@@ -1,7 +1,60 @@
 package simulation.producer.models;
 
-public class Queue implements Runnable{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+
+import simulation.producer.models.observer.Observer;
+import simulation.producer.models.observer.Subject;
+
+public class Queue implements Observer, Runnable{
+    private static int count = 0;
+    private int id;
+    private BlockingQueue<Product> products;
+    private List<Machine> subjects = new ArrayList<Machine>();
+
+    public Queue(){
+        this.id = count++;
+    }
+
+    @Override
+    public void update(Subject subject) {
+        try{
+            // subject.process(products.take());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void run() {
     }   
+    
+    public void addProduct(Product product){
+        try {
+            products.put(product);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public BlockingQueue<Product> getProducts() {
+        return this.products;
+    }
+
+    public void setProducts(BlockingQueue<Product> products) {
+        this.products = products;
+    }
+    
+    public int getId() {
+        return this.id;
+    }
+
+    public void attach (Machine subject){
+        subjects.add(subject);
+    }
+    
+    public void detach (Machine subject){
+        subjects.remove(subject);
+    }
 }
